7f7866c48ceb85fd4eed5921229a5e641247caa4
Talal Alhawaj <202201761@student.polytechnic.bh>
2024-12-19 23:10:12 +0300

    submission ready.

--- tasks/urls.py
+++ tasks/urls.py
@@ -9,14 +9,15 @@ from tasks.views import notifications_view
 
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', CustomLoginView.as_view(), name='login'),  
+    path('home/', views.home, name='home'),  
     path('tasks', views.tasks_view, name='tasks'),
     path('calendar/', views.calendar_view, name='calendar'),
     path('signup/', views.signup_view, name='signup'),
     path('tasks/<int:pk>/', views.task_detail_view, name='task_detail'),
     path('tasks/create/', views.task_create_view, name='task_create'),
     path('tasks/<int:pk>/edit/', views.task_edit_view, name='task_edit'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'), 
     path('logout/', custom_logout, name='logout'),
     path('calendar/data/', views.calendar_data, name='calendar_data'),
     path('tasks/<int:pk>/delete/', delete_task, name='delete_task'),
@@ -26,6 +27,8 @@ urlpatterns = [
     path('tasks/<int:pk>/complete/', views.complete_task, name='complete_task'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
+
+
 
 
 
